b1db4f53e79b6667e4f3a7d0d18a57ae7b7cbb13
Sandipan <kundu.sandipan18@gmail.com>
2024-10-27 02:04:05 +0530

    Updates

---
+++ precache-manifest.5f0f17988d82d7c560674d362224c351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a83ce04f654d12ae085b53c5c538f64",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "a415f4a5c0b12239f042",
+    "url": "/quizapp/static/css/main.4e4d990a.chunk.css"
+  },
+  {
+    "revision": "8e16c8e605e97e8c6930",
+    "url": "/quizapp/static/js/2.5fe24983.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/quizapp/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "a415f4a5c0b12239f042",
+    "url": "/quizapp/static/js/main.b9636d45.chunk.js"
+  },
+  {
+    "revision": "77b980006f21e7a36d41",
+    "url": "/quizapp/static/js/runtime~main.c4b13027.js"
+  }
+]);
